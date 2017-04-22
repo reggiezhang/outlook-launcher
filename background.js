8996419373a5c347abf699ff061bcdd6d35f371e
Reggie Zhang <reggie.zhang@sap.com>
2017-04-22 15:25:04 +0800

    Open existing tab if have

--- background.js
+++ background.js
@@ -1,4 +1,29 @@
-chrome.browserAction.onClicked.addListener(function(activeTab){
+function getOutlookUrl() {
+  return "https://outlook.live.com/owa/";
+}
+
+function isOutlookUrl(url) {
+  // Return whether the URL starts with the Outlook prefix.
+  return url.startsWith(getOutlookUrl());
+}
+function goToInbox() {
+  console.log('Going to inbox...');
+  chrome.tabs.getAllInWindow(undefined, function(tabs) {
+    for (var i = 0, tab; tab = tabs[i]; i++) {
+      if (tab.url && isOutlookUrl(tab.url)) {
+        console.log('Found Outlook tab: ' + tab.url);
+        chrome.tabs.update(tab.id, {selected: true});
+        return;
+      }
+    }
+    console.log('Could not find Outlook tab. Creating one...');
+    chrome.tabs.create({url: getOutlookUrl()});
+  });
+}
+
+chrome.browserAction.onClicked.addListener(goToInbox);
+
+/*chrome.browserAction.onClicked.addListener(function(activeTab){
   var newURL = "https://outlook.live.com/owa/";
   chrome.tabs.create({ url: newURL });
-});
+});*/
