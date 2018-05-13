11168cd464c038baa17f1b9956c56d332ac64c74
Reggie Zhang <reggiezhang@users.noreply.github.com>
2018-05-13 10:55:06 +0800

    Update background.js

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 /*
  * outlook-launcher
- * Copyright (C) 2017 Reggie Zhang <reggy.zhang@gmail.com>
+ * Copyright (C) 2017 Reggie Zhang
  * Licensed under the terms of The GNU Lesser General Public License (LGPLv3):
  * http://www.opensource.org/licenses/lgpl-3.0.html
  *
